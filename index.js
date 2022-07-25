b73b26f751c6fc793a4fb62c7f97f9cca21938c9
yugmani <yugmani@yahoo.com>
2022-07-25 00:14:56 -0700

    merging arrays using for... loop and spread operator

--- index.js
+++ index.js
@@ -24,5 +24,35 @@ console.log(concate123); //["a", "b", "c", "d", "e", "f", "g", "h"]
 // Mergin Arrays with Array.push() method
 // ***************************************
 
-const pushed1 = array1.push(array2);
-console.log(pushed1);
+// array1.push(array2);
+// console.log(array1); //['a', 'b', 'c',  ['d', 'e', 'f']];
+// Not getting a desired result???
+
+//loop through the array2 and push each of them into array1
+for (let i = 0; i < array2.length; i++) {
+  array1.push(array2[i]);
+}
+console.log(array1); //["a", "b", "c", "d", "e", "f"]
+//array1 is mutated
+
+//using Array.map() method
+// array2.map(item=> array3.push(item));
+// console.log(array3);  //["g", "h", "d", "e", "f"]
+
+//using Array.forEach() method
+// array2.forEach((item) => array3.push(item));
+// console.log(array3); //["g", "h", "d", "e", "f"]
+
+//Mergin Arrays with 'Spread Operator'
+// ***********************************
+
+// const spread1 = [array2, array3]
+// console.log(spread1); //[Array[3], Array[2]]
+// Not a desired result???
+
+const spread2 = [...array2, ...array3];
+console.log(spread2); //["d", "e", "f", "g", "h"]
+
+array2.push(...array3);
+console.log(array2); //["d", "e", "f", "g", "h"]
+// array2 is mutated
